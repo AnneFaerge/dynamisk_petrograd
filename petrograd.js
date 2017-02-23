f315db9a90f85c4b5a8eabe5ea01591a96dd11f6
Anne Færgemand Præstegaard <annefaergemandpraestegaard@Annes-MacBook-Air-2.local>
2017-02-23 12:03:44 +0100

    add data navn pris and rabatpris

--- petrograd.js
+++ petrograd.js
@@ -2,15 +2,30 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    visProdukt();
+    //læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktliste);
+
+}
+
+function visProduktliste(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+
+
 }
 
 
-function visProdukt() {
+function visProdukt(produkt) {
+    console.log(produkt);
     //klon produkt_template
-    var klon = document.querySelector("#produkt_template").contentEditable.cloneNode(true);
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
     //indsæt data i klonen
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
